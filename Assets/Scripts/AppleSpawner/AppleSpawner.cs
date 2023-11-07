c590eebedab5350285c39161f11685bf5b8ccc4e
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2023-11-07 16:47:17 +0300

    feat: Created object pooling system and applied to apple spawner

---
+++ Assets/Scripts/AppleSpawner/AppleSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class AppleSpawner : ObjectPooling<GameObject>
+{
+    [SerializeField] private const float MinRangeForXAxis = -1.5f;
+    [SerializeField] private const float MaxRangeForXAxis = 2.4f;
+    [SerializeField] private const float MinRangeForYAxis = 0f;
+    [SerializeField] private const float MaxRangeForYAxis = 3f;
+    void Start()
+    {
+        ObjectPool();
+        StartCoroutine(SpawnApple());
+    }
+
+    private IEnumerator SpawnApple()
+    {
+        while (true)
+        {
+            itemInstantiate = GetItem();
+            GetRandomPositionForApple(itemInstantiate.transform);
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    private void GetRandomPositionForApple(Transform appleTransform)
+    {
+        float randomXCoordinate = Random.Range(MinRangeForXAxis, MaxRangeForXAxis);
+        float randomYCoordinate = Random.Range(MinRangeForYAxis, MaxRangeForYAxis);
+
+        appleTransform.position = new Vector3(randomXCoordinate, randomYCoordinate);
+    }
+}
