0fd1ae26c25cc2e79d90a364a7a1310dd7e6602e
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2023-11-08 15:58:31 +0300

    feat: Created abstrac class for apple states

---
+++ Assets/Scripts/AppleState/Abstract/AppleState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public abstract class AppleState : MonoBehaviour
+{
+   public abstract void ChewApple();
+   public abstract void ClickToApple();
+   public abstract void GrowApple();
+   public abstract void FallApple();
+}
+
